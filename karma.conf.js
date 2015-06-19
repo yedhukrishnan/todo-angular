6a3f238c90d12e5cd985bc9fef88ef286db7f24b
Yedhu Krishnan <yedhukrishnan@yahoo.com>
2015-06-19 16:30:58 +0530

    Use PhantomJS for tests

--- karma.conf.js
+++ karma.conf.js
@@ -15,14 +15,15 @@ module.exports = function(config){
 
     frameworks: ['jasmine'],
 
-    browsers : ['Chrome'],
+    browsers : ['PhantomJS'],
 
     plugins : [
-            'karma-chrome-launcher',
-            'karma-firefox-launcher',
-            'karma-jasmine',
-            'karma-junit-reporter'
-            ],
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-phantomjs-launcher',
+      'karma-jasmine',
+      'karma-junit-reporter'
+    ],
 
     junitReporter : {
       outputFile: 'test_out/unit.xml',
